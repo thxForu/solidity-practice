b7fd9f26c95c5339796ca8048e9f12bd74b15b90
eugene <48453875+thxForu@users.noreply.github.com>
2024-10-08 02:35:39 +0300

    add safeTransfer

--- src/Airdrop/Airdrop.sol
+++ src/Airdrop/Airdrop.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import {IAirdrop} from "src/interfaces/Airdrop/IAirdrop.sol";
 
 contract Airdrop is IAirdrop {
+    using SafeERC20 for IERC20;
     IERC20 public token;
 
     bytes32 public immutable ROOT;
@@ -47,7 +48,7 @@ contract Airdrop is IAirdrop {
         userHasClaimed[msg.sender] = true;
         totalUsers++;
 
-        token.transfer(msg.sender, amount);
+        token.safeTransfer(msg.sender, amount);
         emit TokensClaimed(msg.sender, amount);
     }
 }
